85c4d6a9ac07e541617ae909b3f33a691c07acdb
sage-etcher <sage.code@email.com>
2025-01-21 21:28:26 +0000

    show telescope preview pane even on smaller screens

--- freebsd-devenv/nvim/lua/config/telescope.lua
+++ freebsd-devenv/nvim/lua/config/telescope.lua
@@ -1,5 +1,24 @@
 
--- no config
+
+local telescope = require('telescope')
+
+telescope.setup({
+  defaults = {
+    layout_config = {
+      horizontal = {
+        width  = 0.9,
+        height = 0.9,
+        preview_cutoff = 50,
+      },
+    },
+  },
+  pickers = {
+  },
+  extensions = {
+  }
+})
+
+
 -- keymaps seperate
 
 
