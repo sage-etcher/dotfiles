887b3441d0f90ce9881baaeccec36c30ea18bf58
sage-etcher <sage.message@email.com>
2025-02-06 19:27:35 +0000

    dont set clang-tidy flags in nvim config. require either a project or home .clang-tidy file.
    setting clang-tidy flags in neovim means the flags get sent as
    commandline arguements. clang-tidy evaluates commandline args last,
    meaning that we are unable to overwrite them. instead create a
    .clang-tidy file either in the project root, or in the user's home
    directory (or symlink).

--- freebsd-devenv/nvim/lua/config/nvim_lint.lua
+++ freebsd-devenv/nvim/lua/config/nvim_lint.lua
@@ -13,33 +13,7 @@ end
 local clangtidy = lint.linters.clangtidy
 clangtidy.cmd = 'clang-tidy17'
 clangtidy.args = {
-  '--quiet',
-  '--checks='..
-    -- only do what we tell you to do
-    '-*,'..
-    -- enable group checks
-    'bugprone-*,'..
-    'cert-*,'..
-    'clang-analyzer-core.*,'..
-    'clang-analyzer-nullability.*,'..
-    'clang-analyzer-optin.*,'..
-    'clang-analyzer-security.*,'..
-    'clang-analyzer-unix.*,'..
-    'clang-analyzer-deadcode.*,'..
-    'concurrency-*,'..
-    'misc-*,'..
-    'modernize-*,'..
-    'performance-*,'..
-    'portability-*,'..
-    'readability-*,'..
-    -- enable single checks
-    'cppcoreguidelines-avoid-goto,'..
-    'cppcoreguidelines-avoid-non-const-global-variables,'..
-    'cppcoreguidelines-init-variables,'..
-    -- disable single checks
-    '-misc-include-cleaner,'..
-    '-misc-unused-parameters,'..
-    '-readability-redundant-control-flow'
+  '--quiet'
 }
 
 lint.linters_by_ft = {
