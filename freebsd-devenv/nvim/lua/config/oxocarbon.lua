285d72a6536dc3a864e39a3f4d0c18be68f403a3
sage-etcher <sage.message@email.com>
2025-03-04 16:19:09 +0000

    use vim default color scheme if in xterm

--- freebsd-devenv/nvim/lua/config/oxocarbon.lua
+++ freebsd-devenv/nvim/lua/config/oxocarbon.lua
@@ -6,4 +6,9 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 
+term = os.getenv("TERM")
+if term == "xterm" then
+  vim.cmd("colorscheme vim")
+end
+
 -- end of file
