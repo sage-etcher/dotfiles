76102be01c0b93bdd2d25d833b9440316f253e8f
sage-etcher <sage.message@email.com>
2025-09-30 16:24:16 -0400

    add pyright lsp

--- almalinux9-wsl2/nvim/lua/plugins/lsp.lua
+++ almalinux9-wsl2/nvim/lua/plugins/lsp.lua
@@ -14,11 +14,11 @@ return {
       }
     end,
     config = function()
-      -- lsp setup
       local lspconfig = require('lspconfig')
       local coq = require('coq')
 
       lspconfig.clangd.setup(coq.lsp_ensure_capabilities({}))
+      lspconfig.pyright.setup(coq.lsp_ensure_capabilities({}))
     end,
   },
 
